ba24d093e0b7f2659f3bad111d58d86d0e7e9103
holmofy <hff1996723@163.com>
2024-10-02 20:14:55 +0800

    feat: :sparkles: log time format

--- spring/src/log/config.rs
+++ spring/src/log/config.rs
@@ -31,6 +31,13 @@ pub(crate) struct LoggerConfig {
     #[serde(default)]
     pub format: Format,
 
+    /// Formatters for event timestamps.
+    #[serde(default)]
+    pub time_format: TimeFormat,
+
+    #[serde(default)]
+    pub time_pattern: ChronoTimePattern,
+
     /// Override our custom tracing filter.
     ///
     /// Set this to your own filter if you want to see traces from internal
@@ -91,6 +98,40 @@ pub(crate) enum Format {
     Pretty,
     #[serde(rename = "json")]
     Json,
+}
+
+/// https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/time/index.html
+#[derive(Debug, Default, Clone, JsonSchema, Deserialize)]
+pub(crate) enum TimeFormat {
+    /// Retrieve and print the current wall-clock time.
+    #[default]
+    #[serde(rename = "system")]
+    SystemTime,
+    /// Retrieve and print the relative elapsed wall-clock time since an epoch.
+    #[serde(rename = "uptime")]
+    Uptime,
+    /// Formats local times and UTC times with FormatTime implementations that use the chrono crate.
+    #[serde(rename = "local")]
+    ChronoLocal,
+    /// Formats the current UTC time using a formatter from the chrono crate.
+    #[serde(rename = "utc")]
+    ChronoUtc,
+}
+
+#[derive(Debug, Clone, JsonSchema, Deserialize)]
+#[serde(transparent)]
+pub(crate) struct ChronoTimePattern(String);
+
+impl Default for ChronoTimePattern {
+    fn default() -> Self {
+        Self("%Y-%m-%dT%H:%M:%S".to_string())
+    }
+}
+
+impl ToString for ChronoTimePattern {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
 }
 
 #[derive(Debug, Clone, JsonSchema, Deserialize)]
--- spring/src/log/mod.rs
+++ spring/src/log/mod.rs
@@ -2,10 +2,11 @@ mod config;
 
 use crate::app::AppBuilder;
 use crate::config::ConfigRegistry;
-use config::{Format, LogLevel, LoggerConfig};
+use config::{ChronoTimePattern, Format, LogLevel, LoggerConfig, TimeFormat};
 use std::sync::OnceLock;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::filter::EnvFilter;
+use tracing_subscriber::fmt::time::{ChronoLocal, ChronoUtc, FormatTime, SystemTime, Uptime};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{
@@ -45,6 +46,13 @@ static NONBLOCKING_WORK_GUARD_KEEP: OnceLock<WorkerGuard> = OnceLock::new();
 fn build_logger_layers(config: &LoggerConfig) -> Vec<Box<dyn Layer<Registry> + Sync + Send>> {
     let mut layers = Vec::new();
 
+    let LoggerConfig {
+        format,
+        time_format,
+        time_pattern,
+        ..
+    } = &config;
+
     if let Some(file_config) = &config.file_appender {
         if file_config.enable {
             let file_appender = tracing_appender::rolling::Builder::default()
@@ -59,16 +67,34 @@ fn build_logger_layers(config: &LoggerConfig) -> Vec<Box<dyn Layer<Registry> + S
                 let (non_blocking_file_appender, work_guard) =
                     tracing_appender::non_blocking(file_appender);
                 NONBLOCKING_WORK_GUARD_KEEP.set(work_guard).unwrap();
-                build_fmt_layer(non_blocking_file_appender, &file_config.format, false)
+                build_fmt_layer(
+                    non_blocking_file_appender,
+                    &file_config.format,
+                    time_format,
+                    time_pattern,
+                    false,
+                )
             } else {
-                build_fmt_layer(file_appender, &file_config.format, false)
+                build_fmt_layer(
+                    file_appender,
+                    &file_config.format,
+                    time_format,
+                    time_pattern,
+                    false,
+                )
             };
             layers.push(file_appender_layer);
         }
     }
 
     if config.enable {
-        layers.push(build_fmt_layer(std::io::stdout, &config.format, true));
+        layers.push(build_fmt_layer(
+            std::io::stdout,
+            format,
+            time_format,
+            time_pattern,
+            true,
+        ));
     }
 
     layers
@@ -77,27 +103,49 @@ fn build_logger_layers(config: &LoggerConfig) -> Vec<Box<dyn Layer<Registry> + S
 fn build_fmt_layer<W2>(
     make_writer: W2,
     format: &Format,
+    time_format: &TimeFormat,
+    time_pattern: &ChronoTimePattern,
     ansi: bool,
 ) -> Box<dyn Layer<Registry> + Sync + Send>
 where
     W2: for<'writer> MakeWriter<'writer> + Sync + Send + 'static,
 {
+    match time_format {
+        TimeFormat::SystemTime => config_layer_with_timer(make_writer, format, SystemTime, ansi),
+        TimeFormat::Uptime => config_layer_with_timer(make_writer, format, Uptime::default(), ansi),
+        TimeFormat::ChronoLocal => config_layer_with_timer(
+            make_writer,
+            format,
+            ChronoLocal::new(time_pattern.to_string()),
+            ansi,
+        ),
+        TimeFormat::ChronoUtc => config_layer_with_timer(
+            make_writer,
+            format,
+            ChronoUtc::new(time_pattern.to_string()),
+            ansi,
+        ),
+    }
+}
+
+fn config_layer_with_timer<W2, T>(
+    make_writer: W2,
+    format: &Format,
+    timer: T,
+    ansi: bool,
+) -> Box<dyn Layer<Registry> + Sync + Send>
+where
+    W2: for<'writer> MakeWriter<'writer> + Sync + Send + 'static,
+    T: FormatTime + Sync + Send + 'static,
+{
+    let layer = fmt::Layer::default()
+        .with_ansi(ansi)
+        .with_writer(make_writer)
+        .with_timer(timer);
     match format {
-        Format::Compact => fmt::Layer::default()
-            .with_ansi(ansi)
-            .with_writer(make_writer)
-            .compact()
-            .boxed(),
-        Format::Pretty => fmt::Layer::default()
-            .with_ansi(ansi)
-            .with_writer(make_writer)
-            .pretty()
-            .boxed(),
-        Format::Json => fmt::Layer::default()
-            .with_ansi(ansi)
-            .with_writer(make_writer)
-            .json()
-            .boxed(),
+        Format::Compact => layer.compact().boxed(),
+        Format::Pretty => layer.pretty().boxed(),
+        Format::Json => layer.json().boxed(),
     }
 }
 
