3832a49ab4f6b16427048c029489b909cf3a14da
holmofy <hff1996723@163.com>
2024-10-11 13:17:57 +0800

    feat: :construction: opentelemetry config

--- spring-opentelemetry/src/config.rs
+++ spring-opentelemetry/src/config.rs
@@ -1,8 +1,24 @@
-use schemars::JsonSchema;
+use std::time::Duration;
+
+use opentelemetry_otlp::Compression;
 use serde::Deserialize;
 use spring::config::Configurable;
 
 /// SMTP mailer configuration structure.
-#[derive(Debug, Configurable, Clone, JsonSchema, Deserialize)]
+#[derive(Debug, Configurable, Clone, Deserialize)]
 #[config_prefix = "opentelemetry"]
-pub struct OpenTelemetryConfig {}
+pub struct OpenTelemetryConfig {
+    #[serde(flatten)]
+    otel: Option<OtelExporterConfig>,
+    logs: Option<OtelExporterConfig>,
+    metrics: Option<OtelExporterConfig>,
+    traces: Option<OtelExporterConfig>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct OtelExporterConfig {
+    compression: Option<Compression>,
+    endpoint: Option<String>,
+    headers: Option<String>,
+    timeout: Option<Duration>,
+}
