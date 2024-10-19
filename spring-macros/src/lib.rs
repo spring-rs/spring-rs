add5b163ad51508f44e4ef18b09e507efdac7ff0
holmofy <hff1996723@163.com>
2024-10-19 15:54:20 +0800

    feat: :rocket: upgrade dependency

--- spring-macros/src/lib.rs
+++ spring-macros/src/lib.rs
@@ -1,4 +1,4 @@
-//! spring-macros
+//! [![spring-rs](https://img.shields.io/github/stars/spring-rs/spring-rs)](https://spring-rs.github.io)
 #![doc(html_favicon_url = "https://spring-rs.github.io/favicon.ico")]
 #![doc(html_logo_url = "https://spring-rs.github.io/logo.svg")]
 
