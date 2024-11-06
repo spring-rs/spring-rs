6cc014ba82225e0131c61b0fe0ac557790f91a88
holmofy <hff1996723@163.com>
2024-11-06 15:06:30 +0800

    feat: :construction: add banner

---
+++ spring/src/banner.rs
@@ -0,0 +1,24 @@
+pub const BANNER: &str = r"
+                                   ??                                                                                                       
+                                 .~5BJ                                                                                                      
+                               .~!!PGB?                                                                                                     
+                             :!77!7PGGG!                                                                                                    
+                         .^~!!?77!?PPPPP:                                                                                                   
+                   ..^~7??7!!!?77!Y55555J                                                                                                   
+             .:^777!7YJJ?77!!!?7!?55555Y5^                                                             .                                    
+           :!77?J?7!JYJJ?77!!!?77YYYYYYYY?                .JYY5GBP.                                 !GG.                                    
+         .?J?7?J??77YYJJ?77!!7?7JYYJJJJJYG.              P@!    7@.                                 ~.                                      
+        ^5J?7?YJ??7?5YJJ?77!!7?JYJJJJ??YGB:             ^@@~         :^7P7 .?G#B!  .!5#B:  ~P#B. .^!J5   :?P#G.  7G#B~       .~?YGBGG:      
+       ^PYJ??YYJ??7J5YJJ??7!7JJJJ????J5GGG:              P@@#!      !!&@@!Y#J:B@@.!?:^@@J~BP!?^ ^!5@@P  ?7.?@@^?B?^@@#     .GG^...&@@       
+       55YJ?J5YJJ?7J5YYJ??77JJJ????J5PPPGG.                7&@@J      #@@#J   G@@.   7@@&G.       7@@.     B@@#J  :@@7    J@#    ~@@J       
+      ~P5YJ?Y5YJJ?7J5YYJ??7?JJ?JJY5PPPPPGJ                   7@@B    :@@#.   ^@@^    &@@!         &@B     .@@&:   P@@    J@@.   5@@@.       
+      JP5YJ?55YYJ?7755YJ??JYYY55555PPPPGP.                    G@Y    G@&    ^@B.    ~@@~         ~@@^ .~  5@@.   .@@J  ~ &@# .?#7#@G        
+      YP5YJ?55YYJ?7!J5JJYY5555555555PPGP.             G@BJ7!!YP^    .@@@PYJY5:      #@&          ?@@5Y?. .@@5    :@@GJJ: J@@#GJ .@@~        
+      ?P5YJ?Y55YJ?7!7YY555555555555PPP?                .^~~^.       J@@:::.         ..            ::.     ..      .^:   JGJ:    J@#         
+      ^P5YYJJ55YJ?JY5PPPPPPPPPPPPGGP?.                             :@@:                                               ~@@.     :@&.         
+      .P5YYJ?Y555PPPPPPPPPPPPPGP5?^                           77^~J#Y                                                 ?@@P~::^?B7           
+       55YYY5PGGGPPPPPPPPGPPY7^.                             ^YY?!:                                                    .~JYJ7~.             
+       ?PPGGBGGGGGGGGPPY?~:                                                                                                                 
+       !BBG5YJJ?7!~:..                                                                                                                      
+       ~P:                                                                                                                                  
+       .";
