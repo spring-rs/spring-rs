64eb872db5d65f95a24a5a9f7586a64ecec8d9b3
holmofy <hff1996723@163.com>
2024-12-02 10:51:40 +0800

    docs: :memo: update Utterances theme

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -1,14 +1,23 @@
 // Set darkmode
 document.getElementById('mode').addEventListener('click', () => {
-
-    document.body.classList.toggle('dark');
-    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-  
+  document.body.classList.toggle('dark');
+  var theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+  updateUtterancesTheme('github-' + theme);
 });
-  
+
 // enforce local storage setting but also fallback to user-agent preferences
 if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  
   document.body.classList.add('dark');
-  
+  updateUtterancesTheme('github-dark');
+}
+
+function updateUtterancesTheme(theme) {
+  const iframe = document.querySelector('iframe.utterances-frame');
+  if (iframe) {
+    iframe.contentWindow.postMessage({
+      type: 'set-theme',
+      theme: theme
+    }, 'https://utteranc.es');
+  }
 }
