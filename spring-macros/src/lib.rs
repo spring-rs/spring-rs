f02936cf2a3f979ab9c50c4fba06f79870a9589b
holmofy <hff1996723@163.com>
2025-06-10 21:05:20 +0800

    fix: :rotating_light: remove doctest

--- spring-macros/src/lib.rs
+++ spring-macros/src/lib.rs
@@ -160,32 +160,6 @@ pub fn nest(args: TokenStream, input: TokenStream) -> TokenStream {
 /// # Arguments
 /// - `middleware1`, `middleware2`, etc. - Middleware expressions that will be applied to all routes in the module
 ///
-/// # Example
-/// ```rust
-/// use spring_web::{axum::{middleware::{self, Next}, response::Response}, extractor::Request, Router, middlewares};
-/// use spring_web::middleware::timeout::TimeoutLayer;
-/// use std::time::Duration;
-///
-/// #[middlewares(
-///     middleware::from_fn(my_middleware),
-///     TimeoutLayer::new(Duration::from_secs(10))
-/// )]
-/// mod api {
-///     use spring_web::{get, axum::response::IntoResponse};
-///     #[get("/hello")]
-///     async fn hello() -> impl IntoResponse {
-///         "Hello, world!"
-///     }
-/// }
-///
-/// async fn my_middleware(
-///     request: Request,
-///     next: Next,
-/// ) -> Response {
-///     unimplemented!("do something")
-/// }
-/// ```
-///
 /// This macro generates a router function that applies the specified middleware
 /// to all route handlers defined within the module.
 #[proc_macro_attribute]
