458f8d2664bec0281db81b061b51025a7509aca6
holmofy <hff1996723@163.com>
2025-07-28 17:21:40 +0800

    docs: :memo: add document logo

--- spring-opendal/src/lib.rs
+++ spring-opendal/src/lib.rs
@@ -1,3 +1,6 @@
+//! [![spring-rs](https://img.shields.io/github/stars/spring-rs/spring-rs)](https://spring-rs.github.io/docs/plugins/spring-opendal)
+#![doc(html_favicon_url = "https://spring-rs.github.io/favicon.ico")]
+#![doc(html_logo_url = "https://spring-rs.github.io/logo.svg")]
 pub mod config;
 
 use crate::config::*;
