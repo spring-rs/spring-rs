e44c8c0334ae68552a6830b2562af9dbcbe06829
holmofy <hff1996723@163.com>
2025-08-31 15:14:24 +0800

    chore: :construction: add use case

--- examples/plugin-example/build.rs
+++ examples/plugin-example/build.rs
@@ -1,3 +1,16 @@
 pub fn main() {
-    let _ = spring::config::write_merged_schema_to_file("target/config-schema.json");
+    let _ = spring_grpc::GrpcPlugin;
+    let _ = spring_job::JobPlugin;
+    let _ = spring_mail::MailPlugin;
+    let _ = spring_opendal::OpenDALPlugin;
+    let _ = spring_opentelemetry::OpenTelemetryPlugin;
+    let _ = spring_postgres::PgPlugin;
+    let _ = spring_redis::RedisPlugin;
+    let _ = spring_sea_orm::SeaOrmPlugin;
+    let _ = spring_sqlx::SqlxPlugin;
+    let _ = spring_stream::StreamPlugin;
+    let _ = spring_web::WebPlugin;
+
+    let r = spring::config::write_merged_schema_to_file("../../target/config-schema.json");
+    println!("{r:?}")
 }
