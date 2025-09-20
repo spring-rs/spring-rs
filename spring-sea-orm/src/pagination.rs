1fc57381f185e3294904194609d8a4832e2e229f
holmofy <hff1996723@163.com>
2025-09-20 17:58:31 +0800

    ci: :rotating_light: fix clippy warnning

--- spring-sea-orm/src/pagination.rs
+++ spring-sea-orm/src/pagination.rs
@@ -130,7 +130,10 @@ impl<T> Page<T> {
 
     /// Compute the number of pages for the current page
     fn total_pages(total: u64, size: u64) -> u64 {
-        (total / size) + (total % size > 0) as u64
+        if size == 0 {
+            return 0;
+        }
+        (total / size) + u64::from(!total.is_multiple_of(size))
     }
 
     /// iterator for content
