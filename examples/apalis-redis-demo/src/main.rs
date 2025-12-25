bf1843d0fe86be7c561f0a9da297a666726ae83b
holmofy <hff1996723@163.com>
2025-12-25 22:38:15 +0800

    fix: 📃update apalis-demo

--- examples/apalis-redis-demo/src/main.rs
+++ examples/apalis-redis-demo/src/main.rs
@@ -1,8 +1,3 @@
-use spring_apalis::apalis::{
-    layers::{ErrorHandlingLayer, WorkerBuilderExt as _},
-    prelude::{Context, Monitor, Worker, WorkerBuilder, WorkerFactoryFn as _},
-};
-use spring_apalis::apalis_redis::RedisStorage;
 use serde::{Deserialize, Serialize};
 use spring::{
     app::AppBuilder,
@@ -10,10 +5,16 @@ use spring::{
     plugin::{ComponentRegistry, MutableComponentRegistry},
     tracing, App,
 };
+use spring_apalis::apalis::{
+    layers::{ErrorHandlingLayer, WorkerBuilderExt as _},
+    prelude::{Context, Monitor, Worker, WorkerBuilder, WorkerFactoryFn as _},
+};
+use spring_apalis::apalis_redis::RedisStorage;
 use spring_apalis::{apalis::prelude::*, ApalisConfigurator as _, ApalisPlugin};
 use spring_redis::{Redis, RedisPlugin};
 use spring_web::{
-    axum::response::IntoResponse, extractor::Component, get, WebConfigurator, WebPlugin,
+    axum::response::IntoResponse, extractor::Component, get, middleware::ServiceExt,
+    WebConfigurator, WebPlugin,
 };
 use std::time::Duration;
 
@@ -38,7 +39,7 @@ fn long_running_task_register(app: &mut AppBuilder, monitor: Monitor) -> Monitor
     app.add_component(storage.clone());
 
     let worker = WorkerBuilder::new("long_running")
-        .layer(ErrorHandlingLayer::new())
+        .catch_panic()
         .enable_tracing()
         .rate_limit(5, Duration::from_secs(1))
         .concurrency(2)
